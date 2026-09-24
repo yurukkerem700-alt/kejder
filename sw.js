b1edf3af97f82fda06d324e026c47286cdc365a8
yurukkerem700-alt <yurukkerem700@gmail.com>
2026-09-24 18:11:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Börü: Son Kral - servis çalışanı
 // Önce ağdan yükler (güncellemeler hemen gelir); internet yoksa son kaydedilen sürümü açar.
-const CACHE = 'boru-v2';
+const CACHE = 'boru-v3';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
